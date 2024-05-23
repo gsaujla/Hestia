af8f8b1c3e241e67b8d4106ea96b873464336bea
Mehul <mehulg8142@gmail.com>
2024-05-23 20:56:23 +0530

    Deleted

--- Django/leasing/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import User
-from .models import Property
-from .models import Cart
-
-# Create your views here.
-
